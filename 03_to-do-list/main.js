af1e464e24d5ea1993ddec4a5565f4946cb6eeed
莊哲瑋 <f841u04@gmail.com>
2020-08-12 17:13:52 +0800

    Todolist

--- 03_to-do-list/main.js
+++ 03_to-do-list/main.js
@@ -1,1 +1,42 @@
-// TO DO
+window.addEventListener('DOMContentLoaded', function(){
+  const closeCheck = document.querySelectorAll('li');
+  for(var i=0; i<closeCheck.length; i++){
+    closeCheck[i].addEventListener('click', function(){
+      this.classList.toggle('checked')
+    });
+};
+  const list = document.querySelectorAll('.close')
+  for(var i=0; i<list.length; i++){
+  list[i].addEventListener('click', function(){
+    this.parentNode.remove();
+  });
+  };
+  const createBtn = document.querySelector('.addBtn')
+
+  createBtn.addEventListener('click', function(){
+    const createList = document.createElement('li')
+    const createSpan = document.createElement('span')
+
+    createList.textContent = document.getElementById('input').value
+    createSpan.textContent = 'x'
+    createSpan.className = 'close'
+
+    document.querySelector('ul').appendChild(createList)
+    createList.appendChild(createSpan)
+   
+  const closeCheck = document.querySelectorAll('li');
+  for(var i=0; i<closeCheck.length; i++){
+    closeCheck[i].addEventListener('click', function(){
+      this.classList.toggle('checked')
+    });
+  };
+  const list = document.querySelectorAll('.close')
+  for(var i=0; i<list.length; i++){
+  list[i].addEventListener('click', function(){
+    this.parentNode.remove();
+  });
+  };
+  })
+
+
+})
